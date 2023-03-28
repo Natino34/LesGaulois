344def1290ee7b8e736468ad997057799725bb8a
GNN4587A <GNN4587A@u3-213-02.ens.fsi>
2023-03-28 16:07:59 +0200

    le village gaulois

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,5 +1,6 @@
 package personnages;
 
+import java.util.Iterator;
 
 public class Village {
 	private String nom;
@@ -30,17 +31,30 @@ public class Village {
 		return villageois[indice];
 	}
 	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef.getNom() + " vivent les légenfaires gaulois :");
+		for (int i = 0; i < nbVillageois; i++) {
+			System.out.println("- " + villageois[i].getNom());
+		}
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		Village village = new Village("Village des Irréductibles",30);
 //		Gaulois gaulois = village.trouverHabitant(30);
 //      indice trop grand
 		Chef abraracourcix = new Chef("Abraracourcix",6,village);
+		village.setChef(abraracourcix);
 		Gaulois asterix = new Gaulois("Asterix",8);
 		village.ajouterHabitant(asterix);
 //		Gaulois gaulois = village.trouverHabitant(1);
 //		System.out.println(gaulois);
 //		réponse "null" car aucun gaulois n'est à l'indice 1, asterix est à l'indice 0
+		village.afficherVillageois();
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
 	}
 
 }
